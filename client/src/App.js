9d34065608fa097c8da5a9e090ddd3c5083023ae
Mike Brucker <disposition11@gmail.com>
2019-02-25 21:19:10 -0500

    directory spelling error edit-profile.js to edit-profile

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
-import EditProfile from "./components/edit-profile.js/EditProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 
